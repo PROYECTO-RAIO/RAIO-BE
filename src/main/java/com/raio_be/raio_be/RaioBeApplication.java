435148542880cc5b1b605938218bd1282ac648b5
carocitta <carocitta@gmail.com>
2025-05-30 09:08:43 +0200

    Primer commit - Proyecto Raio en Spring Boot

---
+++ src/main/java/com/raio_be/raio_be/RaioBeApplication.java
@@ -0,0 +1,13 @@
+package com.raio_be.raio_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaioBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaioBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raio_be/raio_be/RaioBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raio_be.raio_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaioBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
