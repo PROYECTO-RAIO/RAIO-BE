67e7bc45415a78833695d52c91f2aa05e9a5d4f7
Marta Ibarra <martaa.ibarra@hotmail.com>
2025-06-05 10:19:04 +0200

    Añadido repository admins

---
+++ src/main/java/com/raio_be/raio_be/repository/AdminsRepository.java
@@ -0,0 +1,9 @@
+package com.raio_be.raio_be.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.raio_be.raio_be.model.Admins;
+
+public interface AdminsRepository extends JpaRepository<Admins, Integer> {
+
+}
