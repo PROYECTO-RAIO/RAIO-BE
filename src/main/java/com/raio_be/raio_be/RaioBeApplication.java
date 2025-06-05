99cb7e9f306a4921b1ffe5a0308d517e8f89457f
Marta Ibarra <martaa.ibarra@hotmail.com>
2025-06-05 10:17:35 +0200

    Añadido variables de entorno & conexion DB

--- src/main/java/com/raio_be/raio_be/RaioBeApplication.java
+++ src/main/java/com/raio_be/raio_be/RaioBeApplication.java
@@ -8,30 +8,21 @@ import io.github.cdimascio.dotenv.Dotenv;
 public class RaioBeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RaioBeApplication.class, args);
 
 		Dotenv dotenv = Dotenv.configure().load();
 
-		checkEnvVariable(dotenv, "SPRING_DATASOURCE_URL");
-		checkEnvVariable(dotenv, "SPRING_DATASOURCE_USERNAME");
-		checkEnvVariable(dotenv, "SPRING_DATASOURCE_PASSWORD");
-		checkEnvVariable(dotenv, "SPRING_DATASOURCE_DRIVER_CLASS_NAME");
-
+		
+		
+		
 		System.setProperty("SPRING_DATASOURCE_URL", dotenv.get("SPRING_DATASOURCE_URL"));
 		System.setProperty("SPRING_DATASOURCE_USERNAME", dotenv.get("SPRING_DATASOURCE_USERNAME"));
 		System.setProperty("SPRING_DATASOURCE_PASSWORD", dotenv.get("SPRING_DATASOURCE_PASSWORD"));
-		System.setProperty("SPRING_DATASOURCE_DRIVER_CLASS_NAME", dotenv.get("SPRING_DATASOURCE_DRIVER_CLASS_NAME"));
 
-		// SpringApplication.run(RaioBeApplication.class, args);
+		
+		SpringApplication.run(RaioBeApplication.class, args);
 		System.out.println("Se ha iniciado con éxito.");
 	}
 
-		private static void checkEnvVariable(Dotenv dotenv, String key) {
-		if (dotenv.get(key) == null || dotenv.get(key).isEmpty()) {
-			throw new IllegalArgumentException("Falta: " + key);
-		}
-	}
+	
 
 }
-
-
