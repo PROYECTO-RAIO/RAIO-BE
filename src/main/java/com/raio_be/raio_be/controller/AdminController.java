ca81eb32bb56f15f46ed5da1daf7a9fcce00fbd5
Marta Ibarra <martaa.ibarra@hotmail.com>
2025-06-06 12:56:49 +0200

    Añadido admin controller

--- src/main/java/com/raio_be/raio_be/controller/AdminController.java
+++ src/main/java/com/raio_be/raio_be/controller/AdminController.java
@@ -1,5 +1,47 @@
 package com.raio_be.raio_be.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.raio_be.raio_be.DTO.AdminDTO;
+import com.raio_be.raio_be.service.AdminService;
+@RestController
+@RequestMapping("api/v1/admins")
 public class AdminController {
-  
+    @Autowired
+    private AdminService adminService;
+
+    @PostMapping
+    public AdminDTO crearAdmin(@RequestBody AdminDTO adminDTO) {
+        return adminService.createAdmin(adminDTO);
+    }
+
+    @GetMapping
+    public List<AdminDTO> getAllAdmins() {
+        return adminService.getAllAdmins();
+    }
+
+    @GetMapping("/{id}")
+    public AdminDTO getAdminById(@PathVariable int id) {
+        return adminService.getAdminById(id);
+    }
+
+    @PutMapping("/{id}")
+    public AdminDTO updateAdmin(@PathVariable int id, @RequestBody AdminDTO adminDTO) {
+        return adminService.updateAdmin(id, adminDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteAdmin(@PathVariable int id) {
+        adminService.deleteAdmin(id);
+    }
 }
