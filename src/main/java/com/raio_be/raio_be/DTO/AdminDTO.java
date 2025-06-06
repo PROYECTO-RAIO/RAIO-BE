f9d7c6ca7c5bbab737bc58aad66423f44d081a7e
Marta Ibarra <martaa.ibarra@hotmail.com>
2025-06-06 12:56:30 +0200

    Añadida entidad AdminDTO

---
+++ src/main/java/com/raio_be/raio_be/DTO/AdminDTO.java
@@ -0,0 +1,18 @@
+package com.raio_be.raio_be.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class AdminDTO {
+  private int id;
+  private String nombreUsuarie;
+  private String contraseña;
+  private String email;
+}
