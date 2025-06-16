ed028423f51a7729a6df6295b597c91f14e2bf47
kat-lev <kat@katleverton.com>
2025-06-16 15:54:20 +0200

    Conectar login con frontend

--- src/main/java/com/raio_be/raio_be/controller/AdminController.java
+++ src/main/java/com/raio_be/raio_be/controller/AdminController.java
@@ -1,8 +1,10 @@
 package com.raio_be.raio_be.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,14 +15,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.raio_be.raio_be.DTO.AdminDTO;
+import com.raio_be.raio_be.model.Admin;
+import com.raio_be.raio_be.repository.AdminRepository;
 import com.raio_be.raio_be.service.AdminService;
 
 import jakarta.validation.Valid;
 @RestController
 @RequestMapping("api/v1/admins")
+@CrossOrigin(origins = "*")
+
 public class AdminController {
     @Autowired
     private AdminService adminService;
+    @Autowired
+    private AdminRepository adminRepository;
 
     @PostMapping
     public AdminDTO createAdmin(@Valid @RequestBody AdminDTO adminDTO) {
@@ -45,5 +53,21 @@ public class AdminController {
     @DeleteMapping("/{id}")
     public void deleteAdmin(@PathVariable Integer id) {
         adminService.deleteAdmin(id);
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestBody Admin loginData) {
+        Optional<Admin> optionalAdmin = adminRepository.findByEmail(loginData.getEmail());
+
+        if (optionalAdmin.isPresent()) {
+            Admin admin = optionalAdmin.get();
+            if (admin.getContraseña().equals(loginData.getContraseña())) {
+                return "Login exitoso";
+            } else {
+                return "Contraseña incorrecta";
+            }
+        } else {
+            return "No existe el usuario con ese email";
+        }
     }
 }
--- src/main/java/com/raio_be/raio_be/repository/AdminRepository.java
+++ src/main/java/com/raio_be/raio_be/repository/AdminRepository.java
@@ -1,9 +1,10 @@
 package com.raio_be.raio_be.repository;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.raio_be.raio_be.model.Admin;
 
 public interface AdminRepository extends JpaRepository<Admin, Integer> {
-
+    Optional<Admin> findByEmail(String email);
 }
